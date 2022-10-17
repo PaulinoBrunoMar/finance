4750eb0e00d6cc06886e6ffea827b1acb11f13bb
Bruno Paulino <marinho.bruno@live.com>
2022-10-17 19:29:59 -0300

    Alteradas formatações de datas e valores para o Brasil.

--- src/components/showExpenses/index.js
+++ src/components/showExpenses/index.js
@@ -17,12 +17,25 @@ function ShowExpenses() {
   }, []);
 
   let total = 0;
+  let totalFormatado = 0;
+  let valor = 0;
+  let valorFormatado = 0;
+  let data_americana;
+  let data_brasileira;
 
   return (
     <>
       <S.List>
         {expenses.map((doc) => {
           total += doc.Valor;
+          totalFormatado = total.toFixed(2);
+          totalFormatado = totalFormatado.replace(".", ",");
+          valor = doc.Valor;
+          valorFormatado = valor.toFixed(2);
+          valorFormatado = valorFormatado.replace(".", ",");
+          data_americana = doc.Vencimento;
+          data_brasileira = data_americana.split('-').reverse().join('/');
+
           return (
             <S.Card key={doc.id}>
               <S.Row>
@@ -39,9 +52,9 @@ function ShowExpenses() {
                   <S.Calendar className="material-symbols-outlined">
                     calendar_month
                   </S.Calendar>
-                  {doc.Vencimento}
+                  {data_brasileira}
                 </S.Span>
-                <S.Value>R$: {doc.Valor}</S.Value>
+                <S.Value>R$: {valorFormatado}</S.Value>
                 <S.Delete
                   className="material-symbols-outlined"
                   onClick={(e) => sweetConfirmForOne(doc.id)}
@@ -52,7 +65,7 @@ function ShowExpenses() {
             </S.Card>
           );
         })}
-        <S.H1>Total: R$ {total}</S.H1>
+        <S.H1>Total: R$ {totalFormatado}</S.H1>
         <S.DelAll onClick={(e) => sweetConfirmForAll()}>
           Delete All Expenses
         </S.DelAll>
